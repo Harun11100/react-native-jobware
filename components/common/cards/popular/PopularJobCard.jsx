19f47f2eeb69b835e34ef3e9c71c689637840136
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-11-03 13:13:25 +0600

    Job data is successfulle fetched from the api just need to add into the JobCard

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 
 import styles from './popularjobcard.style'
+import { TouchableOpacity } from 'react-native'
 
-const PopularJobCard = () => {
+const PopularJobCard = ({item,selectedJob,handleCardPress}) => {
   return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
+    <TouchableOpacity style={styles.container(selectedJob,item)}
+    onPress={()=>handleCardPress}>
+    <TouchableOpacity style={styles.logoContainer(selectedJob,item)}>
+       <Image source={{url:item.employer_logo}}
+        resizeMode='contain'
+        style={styles.logoImage}
+       />
+    </TouchableOpacity>
+
+    </TouchableOpacity>
   )
 }
 
---
+++ hook/useFetch.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: 'GET',
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      'x-rapidapi-key': '6cdaa68426msh9fbe734a4c1ba77p1fbbf4jsn79e14363a607',
+      'x-rapidapi-host': 'jsearch.p.rapidapi.com'
+    },
+    params: { ...query }
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+    } catch (error) {
+      setError(error);
+      alert("An error occurred while fetching data.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [endpoint, query]);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
