d948107047f9b1a24dd279668f81605810eb0458
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-11-03 10:38:36 +0600

    Welcome component UI and font setup is done

--- app/index.js
+++ app/index.js
@@ -1,10 +1,31 @@
-import { View,Text } from "react-native";
+import { useState } from "react";
+import { View,Text,ScrollView,SafeAreaView } from "react-native";
+
+import { Stack,useRouter } from "expo-router";
+import {COLORS,icons,images,SIZES} from '../constants'
+import {Nearbyjobs, Nearbytjobs,Popularjobs,PopularJobs,ScreenHeaderBtn, Welcome} from '../components'
 
 const Home=()=>{
+
+      const router =useRouter();
+
       return(
-            <View>
-                  <Text>Home</Text>
-            </View>
+            <SafeAreaView style={{flex:1,backgroundColor:COLORS.lightWhite}}>
+                  <Stack.Screen options={{headerStyle:{background:COLORS.lightWhite},headerShadowVisible:false,
+                  headerLeft:()=>(<ScreenHeaderBtn inconUrl={icons.menu} dimension='40%'/>),
+                  headerRight:()=>(<ScreenHeaderBtn inconUrl={images.profile} dimension='100%'/>),
+                  headerTitle:"",
+                   }}/>
+                  <ScrollView>
+                        <View
+                        style={{flex:1 ,padding:SIZES.medium}}>
+                          <Welcome/>
+                          <Popularjobs/>
+                          <Nearbyjobs/>
+
+                        </View>
+                  </ScrollView>
+            </SafeAreaView>
       )
 }
 export default Home;
